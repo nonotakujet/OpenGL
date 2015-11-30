6e224d9a6a57cb2cbc7bfe73216e1a4f53a3f92c
nonomura.taku <nonomura.taku@dena.jp>
2015-11-30 19:29:57 +0900

    大地の描画

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -6,7 +6,8 @@
 //  Copyright (c) 2015年 Taku Nonomura. All rights reserved.
 //
 
-// 参考: http://www.natural-science.or.jp/article/20091107221452.php
+// サンプルコード: http://www.natural-science.or.jp/article/20091107221452.php
+// OpenGL入門: http://www.center.nitech.ac.jp/~kenji/Lib/ogl/
 
 #include <iostream>
 #include <GLUT/GLUT.h>
@@ -23,6 +24,7 @@ char WindowTitle[] = "世界の始まり"; // ウィンドウのタイトル
 
 void Initialize(void);
 void Display(void);
+void Ground(void);
 
 // -------------------------
 // メイン関数
@@ -64,6 +66,18 @@ int main(int argc, char *argv[]) {
 void Initialize(void) {
     glClearColor(1.0, 1.0, 1.0, 1.0); // Bufferの初期化 (=これが背景色となる)
     glEnable(GL_DEPTH_TEST); // Z Testを有効にする
+    
+    // 座標変換 (ワールド変換 -> ビュー座標変換(カメラ) -> 射影変換 -> クリッピング)
+    
+    // 射影変換の際に必要な情報
+    gluPerspective(30.0, (double)WindowWidth/(double)WindowHeight, 0.1, 1000.0); // 透視投影の視体積
+    
+    // ビュー座標変換の際に必要な情報
+    gluLookAt(
+        0.0, -100.0, 50.0, // 視点の位置 x, y, z
+        0.0, 100.0, 0.0, // 視界の中心位置座標 x, y, z
+        0.0, 0.0, 1.0 // 視界の上方向のベクトル x, y, z
+    );
 }
 
 // -------------------------
@@ -72,5 +86,41 @@ void Initialize(void) {
 
 void Display(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // カラーバッファとZバッファの消去 (ステンシルバッファ使うときは、別途(GL_STENCIL_BUFFER_BIT)指定する必要あり)
+    
+    Ground();
+    
     glutSwapBuffers(); // glutInitDisplayModel(GLUT_DOUBLE)でダブルバッファリングを可にしているので必要
+}
+
+//----------------------------------------------------
+// 大地の描画
+//----------------------------------------------------
+
+void Ground(void) {
+    double ground_max_x = 300.0;
+    double ground_max_y = 300.0;
+    
+    glColor3d(0.8, 0.8, 0.8); // これから書こうとする命令の色を指定します glColor3d(r, g, b);
+    
+    /*
+     * GL_POINTS : 点を打ちます
+     * GL_LINES : 2点を対にして、その間を直線で結びます
+     * GL_LINE_STRIP : 折れ線を描きます
+     * GL_LINE_LOOP : 折れ線を描きます. 始点と終点の間も結ばれます
+     * GL_POLYGON : 多角形を描きます
+     */
+    
+    glBegin(GL_LINES); // glBegin() ~ glEnd()の間に、その図形の各頂点の座標値を設定する関数を書く.
+    
+    for(double ly = -ground_max_y; ly <= ground_max_y; ly += 10.0){
+        glVertex3d(-ground_max_x, ly, 0);
+        glVertex3d(ground_max_x, ly, 0);
+    }
+    
+    for(double lx = -ground_max_x; lx <= ground_max_x; lx += 10.0){
+        glVertex3d(lx, ground_max_y, 0);
+        glVertex3d(lx, -ground_max_y, 0);
+    }
+    
+    glEnd();
 }
