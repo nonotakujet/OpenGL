5488e13f265ecf31c75bc75d52698fd04c705163
nonomura.taku <nonomura.taku@dena.jp>
2015-12-13 19:28:50 +0900

    地面の上に、いくつかのObjectを表示した (glPushMatrix(), glPopMatrix()の概念を理解した)

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -19,6 +19,30 @@ int WindowHeight = 512; // 生成するウィンドウの高さ
 char WindowTitle[] = "世界の始まり"; // ウィンドウのタイトル
 
 // -------------------------
+// 直方体の定義
+// -------------------------
+
+GLdouble vertex[][3] = {
+    { 0.0, 0.0, 0.0 },
+    { 2.0, 0.0, 0.0 },
+    { 2.0, 2.0, 0.0 },
+    { 0.0, 2.0, 0.0 },
+    { 0.0, 0.0, 30.0 },
+    { 2.0, 0.0, 30.0 },
+    { 2.0, 2.0, 30.0 },
+    { 0.0, 2.0, 30.0 }
+};
+
+int face[][4] = {
+    { 0, 1, 2, 3 },
+    { 1, 5, 6, 2 },
+    { 5, 4, 7, 6 },
+    { 4, 0, 3, 7 },
+    { 4, 5, 1, 0 },
+    { 3, 2, 6, 7 }
+};
+
+// -------------------------
 // 関数プロトタイプ(宣言)
 // -------------------------
 
@@ -67,11 +91,14 @@ int main(int argc, char *argv[]) {
 // -------------------------
 
 void Initialize(void) {
+    
     glClearColor(1.0, 1.0, 1.0, 1.0); // Bufferの初期化 (=これが背景色となる)
+    
     glEnable(GL_DEPTH_TEST); // Z Testを有効にする
     
     // 座標変換 (ワールド変換 -> ビュー座標変換(カメラ) -> 射影変換 -> クリッピング)
     // NOTE : なぜか 通常の順序とは逆に、Viewport変換 -> 射影変換 -> モデルビュー変換の順に設定する.
+    // OpenGL的には、カレント変換行列の生成を行っている.
     
     // 射影変換の際に必要な情報
     glMatrixMode(GL_PROJECTION); //行列モードの設定（GL_PROJECTION : 透視変換行列の設定、GL_MODELVIEW：モデルビュー変換行列）
@@ -80,10 +107,10 @@ void Initialize(void) {
     
     // ビュー座標変換の際に必要な情報
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    glLoadIdentity(); // 行列の初期化
     gluLookAt(
-        0.0, -100.0, 50.0, // 視点の位置 x, y, z
-        0.0, 100.0, 0.0, // 視界の中心位置座標 x, y, z
+        20.0, -200.0, 40.0, // 視点の位置 x, y, z
+        0.0, 0.0, 0.0, // 視界の中心位置座標 x, y, z
         0.0, 0.0, 1.0 // 視界の上方向のベクトル x, y, z
     );
 }
@@ -93,9 +120,51 @@ void Initialize(void) {
 // -------------------------
 
 void Display(void) {
+    
+    // バッファのクリア
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // カラーバッファとZバッファの消去 (ステンシルバッファ使うときは、別途(GL_STENCIL_BUFFER_BIT)指定する必要あり)
     
+    /*
+     * 複数のオブジェクトを描画する際は、Initializeで作成したカレント行列を元に描画する必要があるので、オブジェクトの描画ごとにカレント行列をPushして、描画おわり次第Popする.
+     */
+    
+    // 球
+    glPushMatrix(); // カレント行列をスタックに保存しておく (popされるまでのカレント行列への変更を無視できる)
+    glColor3d(1.0, 0.0, 0.0); // 色の設定
+    glTranslated(0.0, 10.0, 20.0); // 平行移動値の設定 (OpenGL内部では、カレント行列に平行移動行列を乗算して、カレント行列を更新している)
+    glutSolidSphere(4.0, 20, 20);
+    glPopMatrix();
+
+    // 立方体
+    glPushMatrix();
+    glColor3d(0.0, 1.0, 0.0);
+    glTranslated(-20.0, 0.0, 20.0);
+    glutSolidCube(10.0); // 引数: 一辺の長さ
+    glPopMatrix();
+
+    // 円錐
+    glPushMatrix();
+    glColor3d(0.0, 0.0, 1.0);
+    glTranslated(20.0, 100.0, 0.0);
+    glutSolidCone(5.0, 10.0, 20, 20); // 引数: (半径、高さ、Z軸まわりの分割数、Z軸に沿った分割数)
+    glPopMatrix();
+    
+    // 直方体
+    glPushMatrix();
+    glColor3d(0.0, 1.0, 1.0);
+    glTranslated(30.0, 50.0, 0.0); // 平行移動値の設定
+    glBegin(GL_QUADS);
+    for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < 4; i++) {
+            glVertex3dv(vertex[face[j][i]]);
+        }
+    }
+    glEnd();
+    glPopMatrix();
+    
+    glPushMatrix();
     Ground();
+    glPopMatrix();
     
     glutSwapBuffers(); // glutInitDisplayModel(GLUT_DOUBLE)でダブルバッファリングを可にしているので必要
 }
